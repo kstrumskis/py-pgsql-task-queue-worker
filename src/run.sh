e88db29e831fa3ec4bdbed1dc9d598151a2c0e86
Karolis Strumskis <karolis@strumskis.com>
2014-12-17 00:47:39 +0200

    Fixed run.sh script

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-IS_RUNNING=`ps aux|grep "custom_task_worker.py"|grep -v "grep"|wc -l`
+IS_RUNNING=`ps aux|grep "task_worker.py"|grep -v "grep"|wc -l`
 
 if [ $IS_RUNNING -eq 0 ]; then
-    ./custom_task_worker.py >> log &
+    ./task_worker.py >> log &
 fi
